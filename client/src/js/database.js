80137b665f2a52aac5749623b345b36b2dbb2b6b
cmempalmado <cmempalmado@gmail.com>
2022-12-16 07:15:03 -0800

    almost done

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,31 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// Add logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => {
+  console.error('putDb not implemented');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readWrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({ id: 1, value: content });
+  const result = await request;
+  console.log('Data has been saved to the database', result.value);
+};
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+// Add logic for a method that gets all the content from the database
+export const getDb = async () => {
+  console.error('getDb not implemented');
+  const jateDb = await openDB('jate', 1);
+  const tx =  jateDb.transaction('jate', 'readWrite');
+  const store = tx.objectStore('jate');
+  const request = store.get(1);
+  const result = await request
+    if (result) {
+      console.log('Data acquired');
+      return result?.value;
+    } else {
+      console.error('Data not found in database')
+    }
+};
 
 initdb();
